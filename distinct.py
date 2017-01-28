28abcb936936fd7571b7c2e7480f8ae35c230d44
Joe Ryan <joe@clanryan.us>
2017-01-28 09:29:05 -0500

    add unit tests to distinct.py

--- distinct.py
+++ distinct.py
@@ -1,3 +1,5 @@
+import unittest
+
 def distinct(iterable):
 	""" Return unique items by elminiating duplicates.
 	
@@ -15,11 +17,28 @@ def distinct(iterable):
 		yield item
 		seen.add(item)
 	
-def run_distinct():
-	items = [5,7,7,5,6,10,5]
-	for item in distinct(items):
-		print(item)
-		
 
+class TestCases(unittest.TestCase):
+    def test_empty_iterable_raises_StopIteration_Error(self):
+        empty_iter = distinct([])
+        with self.assertRaises(StopIteration):
+            next(empty_iter)
+            
+    def test_list_of_items_with_duplicates_returns_only_distinct_items(self):
+        test_iter = distinct([5, 7, 7, 5, 6, 10, 5])
+        self.assertEqual(5,  next(test_iter))
+        self.assertEqual(7,  next(test_iter))
+        self.assertEqual(6,  next(test_iter))
+        self.assertEqual(10,  next(test_iter))
+        with self.assertRaises(StopIteration):
+            next(test_iter)
+    
+    def test_list_of_items_returns_correct_number_of_items(self):
+        test_iter = distinct([5, 7, 7, 5, 6, 10, 5])
+        count = 0
+        for item in test_iter:
+           count += 1
+        self.assertEqual(4,  count)
+        
 if __name__ == '__main__':
-	run_distinct()
+	unittest.main()
